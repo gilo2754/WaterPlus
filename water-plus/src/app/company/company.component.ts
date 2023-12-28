fc6bea65beae7eb7486473b4ced2d7ba0de60c07
gilo2754 <gilo2754@gmail.com>
2023-12-28 14:43:45 -0800

    test deploy

---
+++ water-plus/src/app/company/company.component.ts
@@ -0,0 +1,12 @@
+// company.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
+})
+export class CompanyComponent {
+  // Puedes agregar lógica adicional aquí si es necesario
+}
